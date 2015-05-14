8ce68b103c59254e51ffba9f3c6e72121125f73c
Terry Weymouth <weymouth@umich.edu>
2015-05-13 21:24:07 -0400

    Correlation Analysis test - not working - snapshot

---
+++ functional_tests/src/test/groovy/pages/analyses/CooralationsAnalysisPage.groovy
@@ -0,0 +1,35 @@
+package pages.analyses
+
+import pages.AnalyzePage
+
+/**
+ * Created by weymouth on 5/13/15.
+ */
+class CooralationsAnalysisPage extends AnalyzePage {
+
+    static at = {
+        selectedAnalysis == 'Correlation Analysis'
+    }
+
+    static content = {
+        def parentContent = AnalyzePage.content
+        parentContent.delegate = delegate
+        parentContent.call()
+
+        analysisWidgetHeader {
+            $('div#analysisWidget h2')
+        }
+
+        variablesBox { $('div#divVariables') }
+
+        runButton { $('input.runAnalysisBtn') }
+
+        resultOutput { $('#analysisOutput form') }
+
+        analysisHeaders { text ->
+            $('span.AnalysisHeader').any {
+                it.text() == text
+            }
+        }
+    }
+}
--- functional_tests/src/test/groovy/tests/analysis/CorrelationAnalysisTests.groovy
+++ functional_tests/src/test/groovy/tests/analysis/CorrelationAnalysisTests.groovy
@@ -1,13 +1,73 @@
 package tests.analysis
 
+import functions.Constants
+
 import org.junit.Test
+
+import pages.AnalyzePage
+import pages.analyses.CooralationsAnalysisPage
+
 import tests.CheckLoginPageAbstract
+
+import static org.hamcrest.Matchers.containsInAnyOrder
+import static org.hamcrest.Matchers.is
 
 public class CorrelationAnalysisTests extends CheckLoginPageAbstract{
 
     @Test
-    void dummyTest() {
-        assert(true);
+    void CorrelationTest() {
+
+        def params = setParams()
+
+        setUpAnalysisSubPage 'Correlation Analysis', params
+
+        runAnalysis params
+
+        verifyPage()
+    }
+
+    private void verifyPage() {
+        def resultHeaders = ["Correlation Table (p-values on top right half, correlation coefficient on bottom left)"]
+        resultHeaders.each {
+            assert analysisHeaders(it)
+        }
+
+    }
+
+    private void runAnalysis(Map params) {
+
+        dragNodeToBox params.variable1, variablesBox
+        dragNodeToBox params.variable2, variablesBox
+        dragNodeToBox params.variable3, variablesBox
+
+        runButton.click()
+        waitFor(8, message: "SurvivalAnalysis RunButton.click() - timed out") { resultOutput } // wait up to 8 seconds for result
+    }
+
+    private setUpAnalysisSubPage(String analysisHeader, Map params) {
+        goToPageMaybeLogin AnalyzePage
+
+        dragNodeToSubset params.subsetNode, 1, 1
+
+        selectAnalysis analysisHeader
+        page CooralationsAnalysisPage
+        verifyAt()
+
+        waitFor { analysisWidgetHeader }
+    }
+
+    private setParams() {
+
+        String diagnosisKey = "${Constants.GSE8581_KEY}Endpoints\\Diagnosis\\"
+
+        return [
+                subsetNode:    Constants.GSE8581_KEY,
+                variable1:      "${Constants.GSE8581_KEY}Endpoints\\FEV1\\",
+                variable2:      "${Constants.GSE8581_KEY}Endpoints\\Forced Expiratory Volume Ratio\\",
+                variable3:      "${Constants.GSE8581_KEY}Subjects\\Age\\"
+                ]
+
     }
 
 }
+
